280b1b789d70dbb070a30bb33344c9dcb6cbbfdd
langerhans <langerhans@users.noreply.github.com>
2018-09-19 15:27:14 +0200

    WB-125: Update all dependencies and make required changes

--- worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BookRepository.java
+++ worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BookRepository.java
@@ -3,11 +3,13 @@ package de.codecentric.psd.worblehat.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByOrderByTitle();
--- worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BorrowingRepository.java
+++ worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BorrowingRepository.java
@@ -3,9 +3,11 @@ package de.codecentric.psd.worblehat.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BorrowingRepository extends JpaRepository<Borrowing, Long> {
 
     @Query("SELECT b from Borrowing b WHERE b.borrowedBook = :book")
