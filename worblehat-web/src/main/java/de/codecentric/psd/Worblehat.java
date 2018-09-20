6369d9a0a76484edcfcd1de9448d802db391571f
philippnowak <philipp.nowak@deutschebahn.com>
2018-09-20 15:31:00 +0200

    Fixed broken build

--- worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
+++ worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
@@ -12,19 +12,11 @@ public class Worblehat {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Worblehat.class, args);
-
-		// this code is basically to (a) demonstrate how to stop a Spring application and (b)
-		// get rid of the SonarQube warning to close the context properly
-		//System.out.println("Enter 'stop' to stop Worblehat.");
+		
 		String line;
 		do {
 			line = scan.nextLine();
 		} while (!"stop".equals(line));
 		applicationContext.close();
 	}
-
-	private Worblehat() {
-		//Private constructor to hide the public one
-	}
-
 }
